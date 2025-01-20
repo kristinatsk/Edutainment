f20ea8df33641ebf21be4c223524f4eb29887c50
kristinatsk <kristinatsk@gmail.com>
2025-01-20 05:38:13 +0300

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Kristina Kostenko on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
